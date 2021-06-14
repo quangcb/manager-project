62fefdeae387f9c4ba07d28223ae912dd106206a
quangcb <79653975+quangcb@users.noreply.github.com>
2021-06-14 18:08:11 +0700

    Add files via upload

---
+++ app-server/target/classes/static/static/js/main.cee81a09.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp-client"]=this["webpackJsonpapp-client"]||[]).push([[0],{15:function(e,t,n){},25:function(e,t,n){"use strict";n.r(t);n(0);var c=n(8),r=n.n(c),s=(n(15),n(2));var i=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hello world jhiufrytrty"})})},p=n(10);r.a.render(Object(s.jsxs)(p.a,{children:[" ",Object(s.jsx)(i,{})]}),document.getElementById("root"))}},[[25,1,2]]]);
+//# sourceMappingURL=main.cee81a09.chunk.js.map
